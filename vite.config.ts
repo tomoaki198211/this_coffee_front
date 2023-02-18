9d7153846ec643cb247c7fb00500c7c0377c377f
tomoaki_narukawa <tomo111911111@gmail.com>
2023-02-19 01:50:26 +0900

    vite.config.tsを修正

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     host: true,
     proxy: {
       "^/api/.*": {
-        target: "http://localhost:3000",
+        target: "http://35.79.178.52",
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, ""),
       },
