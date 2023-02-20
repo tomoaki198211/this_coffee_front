6e3d402de318aa877938681dde4999291a61f89e
tomoaki_narukawa <tomo111911111@gmail.com>
2023-02-20 12:35:19 +0900

    twitter APIの実装

---
+++ src/components/GetTweet.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { ref, reactive } from "vue";
+import axios from "axios";
+import { useAuthStore } from "../stores/auth";
+
+interface Props {
+  name: string;
+}
+const props = defineProps<Props>();
+const tweets = ref([]);
+const authStore = useAuthStore();
+
+async function getTweet(): Promise<void> {
+  const config = {
+    headers: {
+      uid: authStore.uid,
+      "access-token": authStore.access_token,
+      client: authStore.client,
+    },
+  };
+  const data = {
+    tweet: {
+      name: props.name,
+    },
+  };
+  await axios.post("/api/v1/tweets/search", data, config).then((response) => {
+    tweets.value = response.data;
+    console.log(response.data);
+  });
+}
+</script>
+
+<template>
+  <v-card>
+    <v-btn @click="getTweet()" variant="text">ツイッターで評判を見る</v-btn>
+    <v-list>
+      <v-list-item v-for="tweet in tweets" :key="tweet.id"
+        >{{ tweet.text }}
+        <v-divider></v-divider>
+      </v-list-item>
+    </v-list>
+  </v-card>
+</template>
