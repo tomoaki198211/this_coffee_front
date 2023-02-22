5f9d7b06ce3a859d24b647a52cecc2aee26eb5c8
tomoaki_narukawa <tomo111911111@gmail.com>
2023-02-22 11:18:05 +0900

    tsconfig再修正

--- vite.config.ts
+++ vite.config.ts
@@ -17,9 +17,9 @@ export default defineConfig({
       "^/api/.*": {
         // target: "http://localhost:3000",
         //本番環境(s3)では下のproxyが動かずaxiosの部分を書き換えないと動かなかった
-        target: "http://35.79.178.52/api",
+        target: "http://35.79.178.52",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+        // rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
